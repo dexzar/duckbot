36bf80d131b41275278fb8ec32c05b5935a32db9
dexzar <niels.lagcher@gmail.com>
2024-07-16 22:23:11 +0200

    Feature | Added functionality for bald check to only update once a day per user

--- commands/fun/bald.js
+++ commands/fun/bald.js
@@ -1,16 +1,17 @@
 const { SlashCommandBuilder } = require('discord.js')
+const { getCurrentDate } = require('../../utils/getCurrentDate')
+const currentDate = getCurrentDate()
 
 // To do
-/* 1. User can only set bald once per day
-/* 3. Other users can bald check others, if there is no user it won't save
-/* 4. Other users can bald check others, if there is no user limit this to 5x a day
-/* 5. Setup logging for highest of the month
-/*    -> Do this by pushing the user bald score of the day if put in to monthly board
-/*     -> If lower or same, do nothing
-/*     -> If higher, override
-/* 6. Create command to get highest of the month (top5/10?)
-/* 7. Repeat 5/6 for year
-/* 8. Create db for year entries, only add scores of top 10 with name and score
+/* [🗸] 1. User can only set bald once per day
+/* []4. Other users can bald check others, if there is no user limit this to 5x a day
+/* []5. Setup logging for highest of the month
+/*    []-> Do this by pushing the user bald score of the day if put in to monthly board
+/*     []-> If lower or same, do nothing
+/*     []-> If higher, override
+/* []6. Create command to get highest of the month (top5/10?)
+/* []7. Repeat 5/6 for year
+/* []8. Create db for year entries, only add scores of top 10 with name and score
 */
 
 module.exports = {
@@ -25,17 +26,25 @@ module.exports = {
         .setDescription('Enter a username or just leave it empty for yourself.')
     ),
   async execute(interaction, profileData) {
-    const baldScore = profileData.bald
+    const baldScoreData = profileData.bald
     const baldDate = profileData.baldDate
-
-    let bald = Math.floor(Math.random() * 101)
-
     const target = interaction.options.getString('target')
+    let baldScore
+
+    if (baldDate.toISOString().split('T')[0] === currentDate || target) {
+      baldScore = baldScoreData
+    } else {
+      baldScore = Math.floor(Math.random() * 101)
+      profileData.bald = baldScore
+      profileData.baldDate = new Date(currentDate)
+      await profileData.save()
+    }
+
     if (target) {
-      await interaction.reply(`${target} is ${bald}% bald.`)
+      await interaction.reply(`${target} is ${baldScore}% bald.`)
     } else {
       await interaction.reply(
-        `${interaction.user.username} is ${baldScore}% bald.  ${baldDate.toISOString().split('T')[0]}`
+        `${interaction.user.username} is ${baldScore}% bald.`
       )
     }
   }
